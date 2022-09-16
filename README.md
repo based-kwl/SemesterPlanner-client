42183a0799f5045e58f58444383eeab2e1a58c34
PhunnieB <phunniehypnotix@gmail.com>
2022-09-16 14:48:14 -0400

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # SemesterPlanner
 
 Semester Planner app for Android.
+
+To launch:
+- Make sure you have a properly configured android development environment
+1. In a terminal opened in the project folder, run `npx react-native start`
+2. In a new terminal in the same folder, run `npx react-native run-android`
--- SemesterPlanner/App.js
+++ SemesterPlanner/App.js
@@ -78,6 +78,7 @@ const App: () => Node = () => {
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
+            SemesterPlanner :)
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
