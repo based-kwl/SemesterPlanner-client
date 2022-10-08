3dfdd6cec782673a5540edb9d71aa074c1eeb73f
judy <jud59@hotmail.com>
2022-10-08 19:50:51 -0400

    added sign in dummy page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import UserSignIn from './components/Authentication/userSignIn';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Semester Planner App
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/login" element={[<UserSignIn/>]} />
+      </Routes>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Semester Planner App
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
-
---
+++ src/components/Authentication/userSignIn.js
@@ -0,0 +1,9 @@
+
+export default function UserSignIn(){
+
+return (
+    <div>
+        <h1> Semester Planner</h1>
+    </div>
+)
+}
