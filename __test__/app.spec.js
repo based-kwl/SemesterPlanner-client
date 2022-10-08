99df38892c88869e6953b41be3f288a75b4e793c
Jasmin <jasmin.guay@mail.concordia.ca>
2022-10-08 17:14:30 -0400

    Test config + ci modif

--- __test__/app.spec.js
+++ __test__/app.spec.js
@@ -1,13 +1,6 @@
-const request = require("supertest");
-const app = require("../App");
+const sum = require("../src/sum");
 
-describe("Test the root path", () => {
-  test("It should response the GET method", done => {
-    request(app)
-      .get("/")
-      .then(response => {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-  });
+test("It shoud sum to numbers", () => {
+    expect(sum(2,3)).toBe(5);
+
 });
