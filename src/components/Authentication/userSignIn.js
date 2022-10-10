66c25a0f50936be80d0f1173fa7d87f4382727ec
judy <jud59@hotmail.com>
2022-10-10 18:48:51 -0400

    Completed layout of Signin page.

--- src/components/Authentication/userSignIn.js
+++ src/components/Authentication/userSignIn.js
@@ -6,33 +6,61 @@ import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import Card from '@mui/material/Card';
 
 
+
 export default function UserSignIn() {
 
+
     return (
-            <Card variant='outlined' style={{ width:'372px', height:'785px', justifyContent: 'center',backgroundColor: '#E9E3D3', margin:'auto'}}>
-                <p align='center' style={{fontFamily:'Roboto',fontSize: '34px', fontWeight: 'bold', backgroundColor: '#E9E3D3', marginTop:'110px', marginBottom:'16px'}}>
-                    Semester Planner
-                </p>
-                <Card style={{borderRadius:'15px', margin:'auto', alignItems:'center' , display: 'flex', justifyContent: 'center', width:'326px', height:'400px', marginTop:'50px'}} variant='outlined'>
+        <Card variant='outlined' style={{
+            width: '372px',
+            height: '785px',
+            justifyContent: 'center',
+            backgroundColor: '#E9E3D3',
+            margin: 'auto'
+        }}>
+            <p align='center' style={{
+                fontFamily: 'Roboto',
+                fontSize: '34px',
+                fontWeight: 'bold',
+                backgroundColor: '#E9E3D3',
+                marginTop: '110px',
+                marginBottom: '16px'
+            }}>
+                Semester Planner
+            </p>
+            <Card style={{
+                borderRadius: '15px',
+                margin: 'auto',
+                alignItems: 'center',
+                display: 'flex',
+                justifyContent: 'center',
+                width: '326px',
+                height: '400px',
+                marginTop: '50px'
+            }} variant='outlined'>
+                <form>
                     <Box>
-                        <Typography align='center' style={{fontFamily:'Roboto',fontSize: '30px', fontWeight: 'bold'}}>Sign in</Typography>
-                        <div align='center' style={{paddingTop:'16px', paddingBottom:'20px'}}>
-                            <div style={{paddingTop:'16px', paddingBottom:'20px'}}>
-                            <TextField style={{width: '305px'}}
-                                       id='email'
-                                       type='email'
-                                       required
-                                       label="Email"
-                                       variant='outlined'
-                                       InputProps={{
-                                           endAdornment: <InputAdornment
-                                               position="end"><MailOutlineIcon/></InputAdornment>,
-                                       }}
-                            />
+                        <Typography align='center' style={{fontFamily: 'Roboto', fontSize: '30px', fontWeight: 'bold'}}>Sign
+                            in</Typography>
+                        <div align='center' style={{paddingTop: '16px', paddingBottom: '20px'}}>
+                            <div style={{paddingTop: '16px', paddingBottom: '20px'}}>
+                                <TextField
+                                           style={{width: '305px'}}
+                                           id='email'
+                                           type='email'
+                                           required
+                                           label="Email"
+                                           variant='outlined'
+                                           InputProps={{
+                                               endAdornment: <InputAdornment
+                                                   position="end"><MailOutlineIcon/></InputAdornment>,
+                                           }}
+
+                                />
                             </div>
 
                             <div>
-                                <TextField style={{ width: '305px'}}
+                                <TextField style={{width: '305px'}}
                                            id='password'
                                            type='password'
                                            required
@@ -46,20 +74,29 @@ export default function UserSignIn() {
                             </div>
 
                             <div style={{paddingTop: '16px'}}>
-                                <Button variant="contained" style={{paddingTop: '11px',paddingBottom: '11px', width: '305px', backgroundColor:'#912338'}}>Sign
-                                    in</Button>
+                                <Button
+                                    type='submit'
+                                    variant="contained"
+                                    style={{
+                                        paddingTop: '11px', paddingBottom: '11px',
+                                        width: '305px', backgroundColor: '#912338'
+                                    }}>
+                                    Sign in
+                                </Button>
                             </div>
 
-                            <Typography style={{paddingTop:'16px', paddingBottom:'20px'}}>
+                            <Typography style={{paddingTop: '16px', paddingBottom: '20px'}}>
                                 Don't have an account?&nbsp;
-                                 <Link href='/user-registration' style={{ color:'#912338',textDecorationColor:'#912338'}}>
+                                <Link href='/user-registration'
+                                      style={{color: '#912338', textDecorationColor: '#912338'}}>
                                     {'Register here'}
                                 </Link>
                             </Typography>
                         </div>
                     </Box>
-                </Card>
+                </form>
             </Card>
+        </Card>
 
     )
 }
