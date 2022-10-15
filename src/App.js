5f6f924084c0478576a303a75511f4de53a72af4
c0rnkrump3t <maya.mcrae@icloud.com>
2022-10-15 18:25:32 -0400

    calendar view, event list

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import SignIn from './components/Authentication/SignIn';
 import SignUp from './components/Authentication/SignUp';
 import Calendar from './components/Calendar/Calendar';
-import Calendar3 from './components/Calendar/Calendar3';
 
 export default function App() {
     return (
@@ -18,8 +17,7 @@ function AppRoutes() {
         <Routes>
             <Route path="/login" element={<SignIn />} />
             <Route path="/signup" element={<SignUp />} />
-            <Route path="/calendar1" element={<Calendar />} />
-            <Route path="/calendar3" element={<Calendar3 />} />
+            <Route path="/calendar3" element={<Calendar />} />
 
         </Routes>
     );
--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -6,128 +6,78 @@ import Calendar from 'react-calendar';
 import Grid from '@mui/material/Grid';
 import CardContent from '@mui/material/CardContent';
 import '../Calendar/calendar.css'
+import { BackgroundCard, BackgroundCard2, CustomWhiteCard, EventCard } from '../CustomMUIComponents/CustomCards';
 
 
-export default function ShowCalendar() {
-   
+export default function CalendarView() {
     const [date, setDate] = useState(new Date()) // stores date, sets date using Date obj
-   
-    return (
-        <Card variant='outlined' style={{
-            width: '372px',
-            height: '785px',
-            justifyContent: 'center',
-            backgroundColor: '#E9E3D3',
-            margin: 'auto'
-        }}>
+    const mark = [
+        '04-03-2022',
+        '03-03-2022',
+        '05-03-2022'
+    ]
 
-            <Card style={{
-                borderRadius: '15px',
-                margin: 'auto',
-                alignItems: 'center',
-                display: 'flex',
-                justifyContent: 'center',
-                width: '360px',
-                height: '400px',
-                marginTop: '50px'
-            }} variant='outlined'>
-                <Calendar onChange={setDate} value={date} />
-
-            </Card>
-            <Box sx={{ '& button': { m: 1 } }}>
-                <div style={{ paddingTop: '16px', paddingLeft: '30px' }}>
-
-                </div>
-            </Box>
-
-            <Grid>
-
-                <Grid item md={3}>
-
-                    <Card variant='outlined' style={{
-                        borderRadius: '10px',
-                        margin: 'auto',
-                        alignItems: 'center',
-                        display: 'flex',
-                        justifyContent: 'left',
-                        width: '360px',
-                        height: '30px',
-                        marginTop: '5px',
-                        backgroundColor: '#8CC63E',
-                        paddingTop: '8px'
-                    }}>
-
-                        <CardContent>
-
-                            <Typography color="#000000" fontWeight={500} style={{
-                                fontFamily: 'Roboto', alignItems: 'center', display: 'flex',
-
-                            }}>
-                                School
-                            </Typography>
-
-                        </CardContent>
-
-
-                    </Card>
-                </Grid>
-
-
-                <Grid item md={3}>
-
-                    <Card variant='outlined' style={{
-                        borderRadius: '15px',
-                        margin: 'auto',
-                        alignItems: 'center',
-                        display: 'flex',
-                        justifyContent: 'left',
-                        width: '360px',
-                        height: '90px',
-                        marginTop: '10px', overflow: 'auto'
-                    }}>
-                        <CardContent>
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                                14:00-17:00
-                            </Typography>
-
-                            <Typography sx={{ mb: 1.5 }} color="#000000" fontWeight={500} style={{ fontFamily: 'Roboto' }}>
-                                School Meeting
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary" >
-                                Start from screen 16
-                            </Typography>
-                        </CardContent>
-
-                    </Card>
-                    <Card variant='outlined' style={{
-                        borderRadius: '15px',
-                        margin: 'auto',
-                        alignItems: 'center',
-                        display: 'flex',
-                        justifyContent: 'left',
-                        width: '360px',
-                        height: '90px',
-                        marginTop: '10px', overflow: 'auto'
-                    }}>
-                        <CardContent>
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                                14:00-17:00
-                            </Typography>
-
-                            <Typography sx={{ mb: 1.5 }} color="#000000" fontWeight={500} style={{ fontFamily: 'Roboto' }}>
-                                School Meeting
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary" >
-                                Start from screen 16
-                            </Typography>
-                        </CardContent>
-
-                    </Card>
-
-                </Grid>
-
-            </Grid>
-
-        </Card>
+    const celendarMonth = (
+        <React.Fragment>
+            <Calendar onChange={setDate} value={date} 
+           
+            />
+        </React.Fragment>
     )
+
+  
+    const calendarCard = (
+        <React.Fragment>
+            <CustomWhiteCard width='360px' height='400px' marginTop='50px' content={celendarMonth} />
+        </React.Fragment>
+    )
+
+    const eventHeader = (
+        <React.Fragment>
+            <CardContent>
+                <Typography color="#ffffff" fontWeight={500} style={{
+                    fontFamily: 'Roboto', alignItems: 'center', display: 'flex',
+                }}>
+                    School
+                </Typography>
+            </CardContent>
+        </React.Fragment>
+    )
+
+    
+  
+    const eventList = (
+        <CardContent>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                14:00-17:00
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="#000000" fontWeight={500} style={{ fontFamily: 'Roboto' }}>
+                School Meeting
+            </Typography>
+            <Typography variant="body2" color="text.secondary" >
+                Start from screen 16
+            </Typography>
+        </CardContent>
+    )
+
+    
+    const eventCard = (
+        <React.Fragment>
+            <EventCard justifyContent='auto' width='360px' height='30px' marginTop='15px' content={eventHeader} backgroundColor='#8CC63E' />
+            <EventCard justifyContent='left' width='360px' height='90px' marginTop='10px' content={eventList}  />
+            <EventCard justifyContent='left' width='360px' height='90px' marginTop='10px' content={eventList}  />
+
+        </React.Fragment>
+    )
+    const eventContentCard = (
+        <React.Fragment>
+            <EventCard width='360px' height='30px' marginTop='50px' content={eventList} backgroundColor='#8CC63E' />
+        </React.Fragment>
+    )
+
+    const calendarPage = (
+        <BackgroundCard2 width='372px' height='785px' content={calendarCard} content2={eventCard} />
+    )
+
+    return calendarPage;
 }
