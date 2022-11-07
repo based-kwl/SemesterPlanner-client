2228ac9c61b048a78e892b1f2bb9e5d80ccc0170
mnash <m.nashatelsayed@gmail.com>
2022-11-07 00:46:32 -0500

    - added fetch participants and available friends from database - implemented add friends route into frontend - implemented delete friends route into frontend - TODO: test add friends route when fixed in backend - TODO: test delete friends route when implemented in backend

--- src/components/StudyRoom/ParticipantsList.js
+++ src/components/StudyRoom/ParticipantsList.js
@@ -1,56 +1,84 @@
 import * as React from 'react';
-import {useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import {ParticipantCard} from "./StudyRoomCards";
 import Button from '@mui/material/Button';
 import ClearIcon from '@mui/icons-material/Clear';
 import AddIcon from '@mui/icons-material/Add';
 import {PrimaryButton2} from "../CustomMUIComponents/CustomButtons"
+import axios from "axios";
 
 
 export default function ParticipantsList() {
-    const [participants, setParticipants] = useState([
-        "person1",
-        "person2",
-        "person3"
-    ])
+    const studyRoomID = window.location.href.split("/")[window.location.href.split("/").length - 1]
 
-    const [availableFriends, setAvailableFriends] = useState([
-        "person4",
-        "person5",
-        "person6",
-        "person7",
-        "person8",
-        "person9",
-        "person10",
-        "person11",
-        "person12"
-    ])
+    const [participants, setParticipants] = useState([])
+
+    const [errorMessage, setErrorMessage] = useState('')
+
+    const [availableFriends, setAvailableFriends] = useState([])
 
     function handleDelete(index) {
-        let newAvailableFriends = [participants[index], ...availableFriends];
-        setAvailableFriends(newAvailableFriends);
+        // let newAvailableFriends = [participants[index], ...availableFriends];
+        // setAvailableFriends(newAvailableFriends);
+        //
+        // let newParticipants = participants.filter((participant) => {
+        //     return participant !== participants[index];
+        // })
+        // setParticipants(newParticipants);
 
-        let newParticipants = participants.filter((participant) => {
-            return participant !== participants[index];
-        })
-        setParticipants(newParticipants);
+        // TODO: uncomment below code when delete friend route is implemented in backend
+        // axios.post(`http://localhost:5000/room/delete`, {email:availableFriends[index], sID:studyRoomID})
+        //     .then(res => {
+        //         setParticipants(res.data.participants)
+        //     })
+        //     .catch(err => {console.log(`Error: ${err}`); setErrorMessage(`${err}`.substring(44) == 401 ? 'request could not be sent' : `${err}`)});
+        // getParticipants();
     }
 
     function handleAdd(index) {
-        let newParticipants = [...participants, availableFriends[index]];
-        setParticipants(newParticipants);
+        // let newParticipants = [...participants, availableFriends[index]];
+        // setParticipants(newParticipants);
+        //
+        // let newAvailableFriends = availableFriends.filter((availableFriend) => {
+        //     return availableFriend !== availableFriends[index];
+        // })
+        // setAvailableFriends(newAvailableFriends);
 
-        let newAvailableFriends = availableFriends.filter((availableFriend) => {
-            return availableFriend !== availableFriends[index];
-        })
-        setAvailableFriends(newAvailableFriends);
+        // TODO: uncomment below code when add friend route is fixed in backend
+        // axios.post(`http://localhost:5000/room/add`, {email:availableFriends[index], sID:studyRoomID})
+        //     .then(res => {
+        //         setParticipants(res.data.participants)
+        //     })
+        //     .catch(err => {console.log(`Error: ${err}`); setErrorMessage(`${err}`.substring(44) == 401 ? 'request could not be sent' : `${err}`)});
+        // getParticipants();
     }
 
     // TODO: when backend is implemented, update database with participants of the study room using the respective endpoint
     function handleSave() {
         console.log(participants); // remove this line after connecting to backend
         console.log(availableFriends); // remove this line after connecting to backend
+        document.elementFromPoint(0,0).click();
     }
+
+    function getParticipants(){
+        axios.get(`http://localhost:5000/room/fetch/${studyRoomID}`)
+            .then(res => {
+                setParticipants(res.data.participants)
+            })
+            .catch(err => {console.log(`Error: ${err}`); setErrorMessage(`${err}`.substring(44) == 401 ? 'request could not be sent' : `${err}`)});
+    }
+
+    useMemo(getParticipants,[])
+
+    //TODO: check if this can be run ONLY after participants get request is fully completed (currently can see participants in availableFriends for a split second)
+    useMemo(() => {
+        axios.get(`http://localhost:5000/student/email/user_3v5bi5s61v@gmail.com`) //TODO: change user email to local storage email
+            .then(res => {
+                let newAvailableFriends = res.data.friends.filter((email) => !participants.includes(email));
+                setAvailableFriends(newAvailableFriends);
+            })
+            .catch(err => {console.log(`Error: ${err}`); setErrorMessage(`${err}`.substring(44) == 401 ? 'request could not be sent' : `${err}`)});
+    }, [participants])
 
     const participantsList = (
         <>
@@ -84,7 +112,7 @@ export default function ParticipantsList() {
                 position: "fixed",
                 bottom: "30px",
             }}>
-                <PrimaryButton2 content={"Save"} width={"90vw"} onClick={handleSave}/>
+                <PrimaryButton2 content={"Done"} width={"90vw"} onClick={handleSave}/>
             </div>
         </>
     )
