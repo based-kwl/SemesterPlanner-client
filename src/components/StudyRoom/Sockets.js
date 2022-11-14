4149bfbfeb1b0f44a9fd0bfbe04ae82532874428
Jasmin <jasmin.guay@mail.concordia.ca>
2022-11-13 20:44:13 -0500

    Bugfix sockets connection

--- src/components/StudyRoom/Sockets.js
+++ src/components/StudyRoom/Sockets.js
@@ -1,2 +1,10 @@
 import io from "socket.io-client"
-export const socket = io(`${process.env.REACT_APP_BASE_URL}`);
+
+const connectionOptions =  {
+    "force new connection" : true,
+    "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
+    "timeout" : 10000,                  //before connect_error and connect_timeout are emitted.
+    "transports" : ["websocket"]
+};
+
+export const socket = io(`${process.env.REACT_APP_BASE_URL}`, connectionOptions);
