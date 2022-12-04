83b7027f87f7fd298f35dd20170833a43cca3dca
Jasmin <jasmin.guay@mail.concordia.ca>
2022-12-04 04:50:53 -0500

    Bugfix : cancel event now redirect to calendar and fix bugs of eventData initial use State

--- src/components/Calendar/Event/EditEvent.js
+++ src/components/Calendar/Event/EditEvent.js
@@ -15,13 +15,14 @@ import { CalendarDatePicker, CalendarTextField, CalendarTimePicker, UpdateCancel
 export default function EditEvent() {
     const [isRecurrent, setIsRecurrent] = React.useState(false);
     const eventId = window.location.href.split("/")[window.location.href.split("/").length - 1];
-    const [eventData, setEventData] = React.useState();
+    const [eventData, setEventData] = React.useState({});
     const navigate = useNavigate();
     const [eventError, setEventError] = React.useState({ message: "Error, please try again later", hasError: false });
 
     const fetchData = useCallback(() => {
         axios.get(`${process.env.REACT_APP_BASE_URL}events/event/${eventId}`)
             .then((res) => {
+                console.log(res.date);
                 setEventData(res.data)
             }
             ).catch((err) => {
@@ -48,7 +49,7 @@ export default function EditEvent() {
                         data-test="eventEndDate"
                         key={"endDate"}
                         label="Ending date"
-                        value={eventData.endDate}
+                        value={eventData.endDate? eventData.endDate : new Date()}
                         onChange={(e) => setEventData({ ...eventData, endDate: e.$d })}
                     />
                 </LocalizationProvider>
@@ -104,7 +105,7 @@ export default function EditEvent() {
     const editUpdateButtons = (
         <React.Fragment>
             <UpdateCancelButton backgroundColor={'#912338'} content="Update" onClick={() => { handleEvent() }} />
-            <UpdateCancelButton backgroundColor={'#C8C8C8'} content="Cancel" onClick={() => { handleEvent() }} />
+            <UpdateCancelButton backgroundColor={'#C8C8C8'} content="Cancel" onClick={() => { navigate('/calendar') }} />
         </React.Fragment>
     );
 
