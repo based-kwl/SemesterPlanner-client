07d7d3b780df1f171ca323581f2e78b38eb3f1fe
judy <jud59@hotmail.com>
2023-01-11 16:44:56 -0500

    [SP-29] used endpoints to fetch user's course info and study time

--- src/components/ProgressReports/ProgressReportHome.js
+++ src/components/ProgressReports/ProgressReportHome.js
@@ -4,9 +4,42 @@ import * as React from "react";
 import Typography from "@mui/material/Typography";
 import BottomDrawer from "../StudyRoom/BottomDrawer";
 import AssessmentIcon from '@mui/icons-material/Assessment';
+import {StudyRoomCard} from "../StudyRoom/CommonResources";
+import axios from "axios";
+import GetAuthentication from "../Authentication/Authentification";
+import {Stack} from "@mui/system";
 
 
 export default function ProgressReportHome(){
+    const [course, setCourse] = React.useState([])
+    const [time, setTime] = React.useState('')
+    const email = GetAuthentication().email;
+
+
+    React.useEffect(()=>{
+        handleCourseList()
+        handleTotalStudyTime()
+    })
+
+    function handleCourseList(){
+        axios.get(`${process.env.REACT_APP_BASE_URL}student/courses/${email}`)
+            .then(res => {
+                setCourse(res.data.courses);
+            })
+            .catch(err => {
+                console.log('Error', err);
+            })
+    }
+
+    function handleTotalStudyTime(){
+        axios.get(`${process.env.REACT_APP_BASE_URL}student/studyhours/${email}`)
+            .then(res => {
+                setTime(res.data.studyHours);
+            })
+            .catch(err => {
+                console.log('Error', err);
+            })
+    }
 
     const studyEstimator = (
         <React.Fragment>
@@ -16,7 +49,33 @@ export default function ProgressReportHome(){
             <StudyRoomChatCard width='92vw' height='65vh' marginTop='2px' topLeftRadius='0px' topRightRadius='0px'
                                bottomLeftRadius='0px' bottomRightRadius='0px' content={
                 <div style={{overflow: 'auto', height: '60vh'}}>
-                   list of courses taken by students
+                    <h3>Course Workload</h3>
+                    <div style={{overflow: 'auto', height: '32vh'}}>
+                        {course.map((course, index) => (
+                            <div key={index}>
+                                <StudyRoomCard data-test={`${course}`} width={'81vw'} height={'40px'}
+
+                                               content={<Stack direction="row"
+                                                               justifyContent="space-between"
+                                                               spacing={20}>
+                                                   <div style={{
+                                                       width:"70vw",
+                                                       margin: "0px",
+                                                       display: 'flex',
+                                                       justifyItems: 'left'}}>{course.subject} {course.catalog} {course.title} </div>
+                                                   <div style={{
+                                                       width:"11vw",
+                                                       margin: "0px",
+                                                       display: 'flex',
+                                                       justifyItems: 'right'}}>time:{course.studyHours}</div>
+                                               </Stack>}/>
+                            </div>
+                        ))}
+                    </div>
+                    <StudyRoomCard data-test={`${time}`} width={'81vw'} height={'40px'}
+                                   content={<> Total time: {time}
+                                   </>}/>
+
                 </div>
             }/>
 
