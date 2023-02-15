893287ab00979da01956e4858be917192d780739
eyalazimov <eyalazimov@gmail.com>
2023-02-15 14:50:49 -0500

    [#29][SP-29] End-to-End test for Study Hour Estimator - fixed white space

--- src/components/Calendar/Event/EditEvent.js
+++ src/components/Calendar/Event/EditEvent.js
@@ -91,7 +91,7 @@ export default function EditEvent(props) {
 
                     <EventForm eventState={eventData} eventStateSetter={setEventData} courseArray={course}/>
 
-                    <div> {recurrenceSelection()}</div>
+                    <div>{recurrenceSelection()}</div>
 
                 </div>
                 <div>{editUpdateButtons}</div>
