93cfb858080656e78922e60084dc105c4f02899b
Jasmin <jasmin.guay@mail.concordia.ca>
2023-02-16 21:47:09 -0500

    feature/circle-ci-e2e: - setting up e2e configuration

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,11 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({video: false, screenshot: false});
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  video: false,
+  screenshot: false
+});
