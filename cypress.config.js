dd6eccba103ece5244c4d51a84a1a9ed2905d080
Jasmin <jasmin.guay@mail.concordia.ca>
2023-03-01 22:34:21 -0500

    feature/circle-ci-e2e: Adding cypress dependencies manually

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+  e2e: {},
   defaultCommandTimeout: 10000,
-  video: false,
-  screenshot: false
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,14 @@
 import './commands'
 import './friends'
+
+module.exports = (on, config) => {
+    on('before:browser:launch', (browser = {}, launchOptions) => {
+        console.log(launchOptions.args)
+
+        if (browser.name == 'chrome') {
+            launchOptions.args.push('--disable-gpu')
+        }
+
+        return launchOptions
+    })
+}
