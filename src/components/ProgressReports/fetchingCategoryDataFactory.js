563570d07af6f9320e70bb08b09ba34c70ff55bf
mnash <m.nashatelsayed@gmail.com>
2023-03-10 20:41:41 -0500

    [#27][Feature] - attempt to fix sonarcloud Cognitive Complexity code smell

--- src/components/ProgressReports/fetchingCategoryDataFactory.js
+++ src/components/ProgressReports/fetchingCategoryDataFactory.js
@@ -10,73 +10,9 @@ export async function fetchData(api_link, setCourses) {
             const arr = expandEventList(response.data)
 
             if (api_link === 'events-monthly' || api_link === 'events-weekly') {
-                const categoryData = [{name: 'course', colour: '#0072A8', expectedTime: 0}, {
-                    name: 'study',
-                    colour: '#8CC63E',
-                    expectedTime: 0
-                }, {name: 'workout', colour: '#DA3A16', expectedTime: 0}, {
-                    name: 'appointment',
-                    colour: '#DB0272',
-                    expectedTime: 0
-                }]
-
-                arr.forEach((item) => {
-                    switch (item.type) {
-                        case categoryData[0].name:
-                            categoryData[0].expectedTime = categoryData[0].expectedTime + diff_minutes(item.endTime, item.startTime)
-                            break;
-                        case categoryData[1].name:
-                            categoryData[1].expectedTime = categoryData[1].expectedTime + (item.studyHoursConfirmed ? diff_minutes(item.actualEndTime, item.actualStartTime) : diff_minutes(item.endTime, item.startTime))
-                            break;
-                        case categoryData[2].name:
-                            categoryData[2].expectedTime = categoryData[2].expectedTime + diff_minutes(item.endTime, item.startTime)
-                            break;
-                        case categoryData[3].name:
-                            categoryData[3].expectedTime = categoryData[3].expectedTime + diff_minutes(item.endTime, item.startTime)
-                            break;
-                        default:
-                            break;
-                    }
-                })
-                setCourses(categoryData)
+                setCourses(generateEventData(arr))
             } else {
-                const courseList = []
-                const listOfCourses = await handleCourseList()
-                let courseName
-
-                if ("study-events-monthly" === api_link) {
-                    listOfCourses.forEach(item => {
-                        courseList.push({
-                            colour: null,
-                            name: item.subject + item.catalog,
-                            expectedTime: item.studyHours * 60 * 4,
-                            Actual: 0
-                        })
-                    })
-                } else if ("study-events-weekly" === api_link) {
-                    listOfCourses.forEach(item => {
-                        courseList.push({
-                            colour: null,
-                            name: item.subject + item.catalog,
-                            expectedTime: item.studyHours * 60,
-                            Actual: 0
-                        })
-                    })
-                }
-
-                arr.forEach((event) => {
-                    courseName = event.subject + event.catalog
-
-                    courseList.forEach((course) => {
-                        if (course.name === courseName) {
-                            if (!event.studyHoursConfirmed)
-                                course.Actual = course.Actual + diff_minutes(event.endTime, event.startTime)
-                            else
-                                course.Actual = course.Actual + diff_minutes(event.actualEndTime, event.actualStartTime)
-                        }
-                    })
-                })
-                setCourses(courseList)
+                setCourses(await generateStudyData(arr, api_link))
             }
         })
 }
@@ -96,4 +32,78 @@ async function handleCourseList() {
         .catch(err => {
             console.log('Error', err);
         })
+}
+
+function generateEventData(eventsList){
+    const categoryData = [{name: 'course', colour: '#0072A8', expectedTime: 0}, {
+        name: 'study',
+        colour: '#8CC63E',
+        expectedTime: 0
+    }, {name: 'workout', colour: '#DA3A16', expectedTime: 0}, {
+        name: 'appointment',
+        colour: '#DB0272',
+        expectedTime: 0
+    }]
+
+    eventsList.forEach((item) => {
+        switch (item.type) {
+            case categoryData[0].name:
+                categoryData[0].expectedTime = categoryData[0].expectedTime + diff_minutes(item.endTime, item.startTime)
+                break;
+            case categoryData[1].name:
+                categoryData[1].expectedTime = categoryData[1].expectedTime + (item.studyHoursConfirmed ? diff_minutes(item.actualEndTime, item.actualStartTime) : diff_minutes(item.endTime, item.startTime))
+                break;
+            case categoryData[2].name:
+                categoryData[2].expectedTime = categoryData[2].expectedTime + diff_minutes(item.endTime, item.startTime)
+                break;
+            case categoryData[3].name:
+                categoryData[3].expectedTime = categoryData[3].expectedTime + diff_minutes(item.endTime, item.startTime)
+                break;
+            default:
+                break;
+        }
+    })
+
+    return categoryData;
+}
+
+async function generateStudyData(eventsList, api_link) {
+    const courseList = []
+    const listOfCourses = await handleCourseList()
+    let courseName
+
+    if ("study-events-monthly" === api_link) {
+        listOfCourses.forEach(item => {
+            courseList.push({
+                colour: null,
+                name: item.subject + item.catalog,
+                expectedTime: item.studyHours * 60 * 4,
+                Actual: 0
+            })
+        })
+    } else if ("study-events-weekly" === api_link) {
+        listOfCourses.forEach(item => {
+            courseList.push({
+                colour: null,
+                name: item.subject + item.catalog,
+                expectedTime: item.studyHours * 60,
+                Actual: 0
+            })
+        })
+    }
+
+    eventsList.forEach((event) => {
+        courseName = event.subject + event.catalog
+
+        courseList.forEach((course) => {
+            if (course.name === courseName) {
+                if (!event.studyHoursConfirmed)
+                    course.Actual = course.Actual + diff_minutes(event.endTime, event.startTime)
+                else
+                    course.Actual = course.Actual + diff_minutes(event.actualEndTime, event.actualStartTime)
+            }
+        })
+    })
+
+    return courseList;
 }
