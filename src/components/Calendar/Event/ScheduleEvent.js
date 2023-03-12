6c79bf1210ec803c5d07f4426d5cfbeaa44c2801
judy <jud59@hotmail.com>
2023-03-11 21:34:55 -0500

    [SP-20][Feature] - can add and remove courses from the schedule tab

--- src/components/Calendar/Event/ScheduleEvent.js
+++ src/components/Calendar/Event/ScheduleEvent.js
@@ -1,4 +1,3 @@
-// import GetAuthentication from "../../Authentication/Authentification";
 import * as React from "react";
 import {Stack} from "@mui/material";
 import {PrimaryButton2} from "../../CustomMUIComponents/CustomButtons";
@@ -9,62 +8,133 @@ import {CourseCard} from "../../StudyRoom/CommonResources";
 import Button from "@mui/material/Button";
 import ClearIcon from "@mui/icons-material/Clear";
 import AddIcon from '@mui/icons-material/Add';
+import axios from "axios";
+import GetAuthentication from "../../Authentication/Authentification";
+import moment from "moment/moment";
 
 
 export default function ScheduleEvent() {
-    // const email = GetAuthentication().email;
-    // const dummyCourseList = ['SOEN 321', 'COMP 435', 'SOEN 490', 'ENGR 213', 'ELEC 275']
-    const[courseList, setCourseList] = React.useState([])
-    const [schedule, setSchedule] = React.useState({
-        subject:'',
-        catalog:'',
+    const username = GetAuthentication().username;
+    const [error, setError] = React.useState('')
+    const [validateCourse, setValidateCourse] = React.useState(false)
+    const [schedule, setSchedule] = React.useState([{
+        username: username,
+        eventHeader:'',
+        description:'',
+        recurrence:'weekly',
+        link:'',
+        type:'course',
+        subject:'ex:SOEN',
+        catalog:'ex:385',
         day1:'Mo',
         day2:'Mo',
         startTime:new Date(),
-        endTime:new Date()
-    });
+        endTime:new Date(),
+        actualStartTime:new Date(),
+        actualEndTime:new Date(),
+        startDate:'',
+        endDate:''
+    }]);
 
     function handleEvent() {
-        console.log('list of events to validate:',schedule)
+        setError('')
+        schedule.forEach((course)=>{
+            axios.get(`${process.env.REACT_APP_BASE_URL}opendata/course/${course.subject}/${course.catalog}`)
+                .then((res) => {
+                    if(res.data){
+                        console.log(res.data.title)
+                        console.log('correct',course.subject, course.catalog)
+                        course.eventHeader = res.data.subject + ' ' + res.data.catalog
+                        course.description = res.data.title
+                        course.startDate = moment("DD/MM/YYYY").toDate()
+
+                    }else{
+                        console.log('not correct', course.subject, course.catalog)
+                        const error = 'invalid course: '+ course.subject+ ' '+course.catalog
+                        setError(error)
+                        setValidateCourse(false)
+                    }
+                })
+        })
+        console.log(schedule)
+        // if(validateCourse){
+        //     schedule.forEach((course)=>{
+        //         const newSchedule = [...schedule]
+        //         axios.post(`${process.env.REACT_APP_BASE_URL}events/add`, schedule)
+        //             .then((res) => {
+        //                 document.elementFromPoint(0, 0).click();
+        //             })
+        //             .catch(err => {
+        //
+        //             });
+        //
+        //     })
+        // }
     }
 
     function handleCancel() {
         document.elementFromPoint(0, 0).click();
     }
 
-    function handleDayChange(index, e) {
-        console.log(index,e.target.name, e.target.value)
+    function handleDayChange(course, e) {
+        const newSchedule = [...schedule]
         if(e.target.name ==='day1'){
-            setSchedule({...schedule,day1:e.target.value})
+            course.day1 = e.target.value
         }else{
-            setSchedule({...schedule,day2:e.target.value})
+            course.day2 = e.target.value
         }
-        console.log(schedule)
+        // newSchedule.push(course)
+        setSchedule(newSchedule)
     }
-    function handleTimeChange(){
-
+    function handleTimeChange(course, e){
+        const newDate = new Date()
+        newDate.setHours(e.target.value.split(':')[0])
+        newDate.setMinutes(e.target.value.split(':')[1])
+        const newTime = [...schedule]
+        if(e.target.id === 'startTime'){
+            course.startTime = newDate
+        }else{
+            course.endTime = newDate
+        }
+        setSchedule(newTime)
     }
     function handleDelete(index){
-        const updatedList = courseList.filter((_, i) => i !== index);
-        setCourseList(updatedList);
+        const updatedList = schedule.filter((_, i) => i !== index);
+        setSchedule(updatedList)
+
     }
     function handleAdd(){
-        setCourseList([...courseList, "sample"]);
-        console.log(courseList)
+        const addCourse = {
+            subject:'',
+            catalog:'',
+            day1:'Mo',
+            day2:'Mo',
+            startTime:new Date(),
+            endTime:new Date()
+        }
+        const newSchedule = [...schedule]
+        newSchedule.push(addCourse)
+        setSchedule(newSchedule);
+
     }
-    function handleCourse(e){
-        setSchedule({...schedule, subject:e.target.value});
-        console.log(schedule)
+    function handleCourse(e, course){
+        const newSchedule = [...schedule]
+        if(e.target.id === 'subject'){
+            course.subject = e.target.value
+        }else{
+            course.catalog = e.target.value
+        }
+        setSchedule(newSchedule)
     }
-    const SelectDay = (index, onChange, name) =>{
+    const SelectDay = (course, onChange, name) =>{
         return(
             <TextField
                 name={name}
                 id='day'
                 label="day"
                 select
-                onChange={(e)=>onChange(index,e)}
-                defaultValue={schedule.day1}
+                onChange={(e)=>onChange(course,e)}
+                value={name==='day1'?course.day1: course.day2}
                 sx={{
                     width: '15vw', color: 'black',
                     "& .css-11u53oe-MuiSelect-select-MuiInputBase-input-MuiOutlinedInput-input":
@@ -121,7 +191,7 @@ export default function ScheduleEvent() {
 
     const coursesFound = (
         <React.Fragment>
-            {courseList.map((course, index) => (
+            {schedule.map((course, index) => (
                 <div key={index} style={{width: '98%', textAlign: 'center', justifyContent:'center', margin:'1%'}}>
                 <CourseCard width={'100%'} height={'fit-content'}
                                content={
@@ -130,24 +200,20 @@ export default function ScheduleEvent() {
                                            <TextField required
                                                       data_test="courseSubject"
                                                       id="subject"
-                                                      label="subject"
-                                                      value={schedule.subject}
-                                                      defaultValue="EX: SOEN"
+                                                      label="course code"
+                                                      value={course.subject}
                                                       size="small"
                                                       margin="none"
-                                                      onChange={(e)=>{handleCourse(e,index)}}
+                                                      onChange={(e)=>{handleCourse(e,course)}}
                                            />
                                            <TextField required
                                                       data_test="courseCatalog"
                                                       id="catalog"
-                                                      label="catalog"
-                                                      value={schedule.catalog}
-                                                      defaultValue="EX: 385"
+                                                      label="course number"
+                                                      value={course.catalog}
                                                       size="small"
                                                       margin="none"
-                                                      // onChange={(e) => {
-                                                      //     setSchedule({...schedule, catalog: e.target.value})}}
-                                                      onChange={(e)=>{handleCourse(index)}}
+                                                      onChange={(e)=>{handleCourse(e, course)}}
                                            />
                                            <Button
                                                sx={{marginRight:'0px'}}
@@ -168,13 +234,13 @@ export default function ScheduleEvent() {
                                        </Stack>
 
                                        <Stack direction='row' spacing={1} marginTop={1}  justifyContent='space-between'>
-                                           {SelectDay(index, handleDayChange, 'day1')}
-                                           {SelectDay(index, handleDayChange, 'day2')}
+                                           {SelectDay(course, handleDayChange, 'day1')}
+                                           {SelectDay(course, handleDayChange, 'day2')}
                                            <TextField
                                                id="startTime"
                                                label="Start Time"
                                                type="time"
-                                               value={getTime(new Date())}
+                                               value={getTime(course.startTime)}
                                                sx={{
                                                    width: '29%',
                                                    "& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input":
@@ -184,14 +250,14 @@ export default function ScheduleEvent() {
                                                        }
                                                }}
                                                data-test="eventStartTime"
-                                               onChange={handleTimeChange}
+                                               onChange={(e) => {handleTimeChange(course,e)}}
                                                margin="normal"
                                            />
                                            <TextField
                                                id="endTime"
                                                label="End Time"
                                                type="time"
-                                               value={getTime(new Date())}
+                                               value={getTime(course.endTime)}
                                                sx={{
                                                    width: '29%',
                                                    "& .css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input":
@@ -202,7 +268,7 @@ export default function ScheduleEvent() {
                                                }}
 
                                                data-test="eventStartTime"
-                                               onChange={handleTimeChange}
+                                               onChange={(e) => {handleTimeChange(course,e)}}
                                                margin="normal"
                                            />
                                        </Stack>
@@ -226,10 +292,11 @@ export default function ScheduleEvent() {
     )
     return (
         <React.Fragment>
-            <div style={{  width:'98vw', height:'55vh', overflowY:'auto', marginBottom:'10px', border:'1px solid black', marginTop:'10px'}}>
+            <div style={{  width:'98vw', height:'50vh', overflowY:'auto', marginBottom:'10px', border:'1px solid black', marginTop:'10px'}}>
                 {coursesFound}
                 {addCourse}
             </div>
+            <div style={{color: 'red', height: '5vh'}}>{error}</div>
             {buttons}
         </React.Fragment>
     )
