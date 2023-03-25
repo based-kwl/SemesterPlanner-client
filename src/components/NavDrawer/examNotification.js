dd7f3344dc848b8c0f2089d644b7047f830cdaed
RamB98 <ramzi2009@live.ca>
2023-03-25 09:51:51 -0400

    made the study  timeslots reactive

--- src/components/NavDrawer/examNotification.js
+++ src/components/NavDrawer/examNotification.js
@@ -1,20 +1,24 @@
 import * as React from "react";
-import {InputAdornment, Stack} from "@mui/material";
-import {PrimaryButton2} from "../CustomMUIComponents/CustomButtons";
+import { InputAdornment, Stack } from "@mui/material";
+import { PrimaryButton2 } from "../CustomMUIComponents/CustomButtons";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
-import {TimeCard} from "../CustomMUIComponents/CustomCards";
+import { TimeCard } from "../CustomMUIComponents/CustomCards";
 import Button from "@mui/material/Button";
 import AddIcon from '@mui/icons-material/Add';
-import {getTime} from "../Calendar/CommonFunctions";
+import CheckIcon from '@mui/icons-material/Check';
+import { getTime } from "../Calendar/CommonFunctions";
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import GetAuthentication from "../Authentication/Authentification";
 import axios from "axios";
-import {delay} from "../CommonHelperFunctions/CommonHelperFunctions";
+import { delay } from "../CommonHelperFunctions/CommonHelperFunctions";
+import { getHoursBetweenTimestamps } from "./examNotificationFactory";
 
-export default function ExamNotification(props){
-    const [timeSlot, setTimeSlot] = React.useState(60)
+export default function ExamNotification(props) {
+    const [studyTimes, setStudyTimes] = React.useState([[]])
+    const [timeSlot, setTimeSlot] = React.useState([])
+    const [timePeriod, setTimePeriod] = React.useState(60)
     const [exams] = React.useState(props.examData)
     const [availability, setAvailability] = React.useState({
         startTime: new Date(),
@@ -22,52 +26,55 @@ export default function ExamNotification(props){
     })
     const options = { month: "long" };
     let date = new Date(exams.startDate)
-    let date2 = new Intl.DateTimeFormat('en-us',options).format(date).toUpperCase()
+    let date2 = new Intl.DateTimeFormat('en-us', options).format(date).toUpperCase()
     let date3 = date.getDate()
 
 
-    React.useEffect(()=>{
+    React.useEffect(() => {
+        getHoursBetweenTimestamps("2023-03-24", "2023-03-31", setTimeSlot, timePeriod)
+        console.log(timeSlot)
         initTimes()
-    },[])
+    }, [timePeriod])
 
-    function initTimes(){
+    function initTimes() {
         const newInitTime =
-            {
-                startTime: new Date(),
-                endTime: new Date()
-            }
-        newInitTime.startTime.setHours(9,0,0,0)
-        newInitTime.endTime.setHours(18,0,0,0)
+        {
+            startTime: new Date(),
+            endTime: new Date()
+        }
+        newInitTime.startTime.setHours(9, 0, 0, 0)
+        newInitTime.endTime.setHours(18, 0, 0, 0)
         setAvailability(newInitTime)
     }
-    function handleCancel(){
+    function handleCancel() {
         document.elementFromPoint(0, 0).click();
     }
-    function handleDismiss(){
+    function handleDismiss() {
         exams.studyHoursConfirmed = true
 
         axios.post(`${process.env.REACT_APP_BASE_URL}events/update`, exams)
-                .then(() => {
-                })
-                .catch(err => {
-                    console.log(err)
-                });
-        delay(200).then(()=>{
+            .then(() => {
+            })
+            .catch(err => {
+                console.log(err)
+            });
+        delay(200).then(() => {
             window.location.reload()
         })
     }
 
+
     function handleEvent() {
         exams.forEach((exam) => {
             const eventDay = {
-                username:GetAuthentication().username,
+                username: GetAuthentication().username,
                 eventHeader: exam.subject + ' ' + exam.data.catalog,
-                description:'study for exam',
+                description: 'study for exam',
                 recurrence: 'once',
                 link: '',
                 type: 'exam',
                 subject: exam.subject,
-                catalog:exam.catalog,
+                catalog: exam.catalog,
                 startTime: exam.startTime.toISOString(),
                 endTime: exam.endTime.toISOString(),
                 actualStartTime: new Date().toISOString(),
@@ -81,7 +88,7 @@ export default function ExamNotification(props){
                 })
         })
     }
-    function handleTimeChange(e){
+    function handleTimeChange(e) {
         console.log(e.target.id)
         const newDate = new Date()
         newDate.setHours(e.target.value.split(':')[0])
@@ -89,17 +96,14 @@ export default function ExamNotification(props){
 
         console.log(newDate)
         if (e.target.id === 'startTime') {
-            setAvailability({...availability, startTime: newDate})
+            setAvailability({ ...availability, startTime: newDate })
         } else {
-            setAvailability({...availability, endTime: newDate})
+            setAvailability({ ...availability, endTime: newDate })
         }
     }
-    function handleMenu(e){
-        setTimeSlot(e.target.value)
+    function handleMenu(e) {
+        setTimePeriod(e.target.value)
         console.log(timeSlot)
-    }
-    function handleAddStudyTimeSlots(){
-
     }
 
     const startTime = (
@@ -113,9 +117,9 @@ export default function ExamNotification(props){
                 shrink: true,
             }}
             sx={{
-            "& css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input":
+                "& css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input":
                 {
-                    padding:'0',
+                    padding: '0',
                 }
             }}
             margin="none"
@@ -133,9 +137,9 @@ export default function ExamNotification(props){
             onChange={handleTimeChange}
             sx={{
                 "& css-1t8l2tu-MuiInputBase-input-MuiOutlinedInput-input":
-                    {
-                        padding:'0',
-                    }
+                {
+                    padding: '0',
+                }
 
             }}
             margin="none"
@@ -149,13 +153,10 @@ export default function ExamNotification(props){
             margin="none"
             select
             onChange={handleMenu}
-            value={timeSlot}
+            value={timePeriod}
         >
             <MenuItem value={60}>
                 60 min
-            </MenuItem>
-            <MenuItem value={45}>
-                45 min
             </MenuItem>
             <MenuItem value={30}>
                 30 min
@@ -163,90 +164,138 @@ export default function ExamNotification(props){
         </TextField>
     )
     const examName = (
-        <div align='center' style={{marginBottom:'5px'}}>
-        <Typography variant="h5"> {exams.subject+' '+exams.catalog} EXAM ON {date2} {date3}</Typography>
+        <div align='center' style={{ marginBottom: '5px' }}>
+            <Typography variant="h5"> {exams.subject + ' ' + exams.catalog} EXAM ON {date2} {date3}</Typography>
         </div>
     )
 
     const showAvailability = (
         <React.Fragment>
-            <div style={{width:'97vw', marginBottom:'5px', paddingLeft:'7px'}}>
-            <Typography> show availability from <p style={{alignItems:'center'}}>{startTime} to {endTime}</p></Typography>
+            <div style={{ width: '97vw', marginBottom: '5px', paddingLeft: '7px' }}>
+                <Typography> show availability from <p style={{ alignItems: 'center' }}>{startTime} to {endTime}</p></Typography>
                 <Typography> display slots of {slotMenu} </Typography>
             </div>
 
         </React.Fragment>
     )
 
-    const timeDisplay = (
-        <TimeCard width={'100%'}
-                       content={<>
-                           <Stack direction="row" alignItems='center' justifyContent='space-around' spacing={1}>
-                               <Typography variant="subtitle1">TUE FEB 21</Typography>
-                               <TextField
-                                   id="courseStart"
-                                   type="text"
-                                   value='00:00'
-                                   size='small'
-                                   disabled
-                                   InputProps={{
-                                       endAdornment:(<InputAdornment position='end'>
-                                           <AccessTimeIcon/>
-                                           </InputAdornment>
-                                           ),
-                                   }}
-                                   sx={{
-                                       width:'25vw',
-                                       "& .css-o9k5xi-MuiInputBase-root-MuiOutlinedInput-root":
-                                           {
-                                               paddingRight:'7px',
-                                           }
-                                   }}
-                                   data-test="eventEndTime"
-                                   margin="none"
-                               />
-                               <TextField
-                                   id="courseStart"
-                                   type="text"
-                                   value='00:00'
-                                   size='small'
-                                   disabled
-                                   InputProps={{
-                                       endAdornment:(<InputAdornment position='end'>
-                                               <AccessTimeIcon/>
-                                           </InputAdornment>
-                                       ),
-                                   }}
-                                   sx={{
-                                       width:'25vw',
-                                       "& .css-o9k5xi-MuiInputBase-root-MuiOutlinedInput-root":
-                                           {
-                                               paddingRight:'7px',
-                                           }
-                                   }}
-                                   data-test="eventEndTime"
-                                   margin="none"
-                               />
-                               <Button
-                                   data-test={`accept-request-`}
-                                   variant="text"
-                                   onClick={() => handleAddStudyTimeSlots()}><AddIcon
-                                   style={{color: '#057D78', margin:'0px'}}/>
-                               </Button>
-                           </Stack>
-                       </>}>
-        </TimeCard>
-    )
+
+
+
+
+    const handleAddStudyTimeSlots = (startTime) => {
+        const endTime = new Date(startTime);
+        endTime.setMinutes(endTime.getMinutes() + (timePeriod === 60 ? 60 : 30));
+        setStudyTimes([...studyTimes, [startTime, endTime]]);
+        console.log(studyTimes)
+    };
+    const handleRemoveStudyTimeSlots = (startTime) => {
+        setStudyTimes(studyTimes.filter(time => time[0] !== startTime));
+        console.log(studyTimes)
+    };
+    const timeDisplay = timeSlot
+        .filter((startTime) => {
+            const date = new Date(startTime);
+            const startHour = availability.startTime.getHours();
+            const endHour = availability.endTime.getHours();
+            const currentHour = date.getHours();
+            return currentHour >= startHour && currentHour < endHour;
+        })
+        .map((startTime, index) => {
+            const date = new Date(startTime);
+            const dateString = `${date.toLocaleString('default', { weekday: 'short' }).toUpperCase()} ${date.toLocaleString('default', { month: 'short' }).toUpperCase()} ${date.getDate()}`;
+            const endTime = new Date(startTime);
+            endTime.setMinutes(endTime.getMinutes() + (timePeriod === 60 ? 60 : 30));
+            const endTimeString = endTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+            const startTimeString = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+            const isTimeSelected = studyTimes.some(time => time[0] === startTime);
+            return (
+                <TimeCard
+                    key={index}
+                    width={'100%'}
+                    content={
+                        <>
+                            <Stack direction="row" alignItems="center" justifyContent="space-around" spacing={1}>
+                                <Typography variant="subtitle1">{dateString}</Typography>
+                                <TextField
+                                    id="courseStart"
+                                    type="text"
+                                    value={startTimeString}
+                                    size="small"
+                                    disabled
+                                    InputProps={{
+                                        endAdornment: (
+                                            <InputAdornment position="end">
+                                                <AccessTimeIcon />
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                    sx={{
+                                        width: '25vw',
+                                        '& .css-o9k5xi-MuiInputBase-root-MuiOutlinedInput-root': {
+                                            paddingRight: '7px',
+                                        },
+                                    }}
+                                    data-test="eventEndTime"
+                                    margin="none"
+                                />
+                                <TextField
+                                    id="courseStart"
+                                    type="text"
+                                    value={endTimeString}
+                                    size="small"
+                                    disabled
+                                    InputProps={{
+                                        endAdornment: (
+                                            <InputAdornment position="end">
+                                                <AccessTimeIcon />
+                                            </InputAdornment>
+                                        ),
+                                    }}
+                                    sx={{
+                                        width: '25vw',
+                                        '& .css-o9k5xi-MuiInputBase-root-MuiOutlinedInput-root': {
+                                            paddingRight: '7px',
+                                        },
+                                    }}
+                                    data-test="eventEndTime"
+                                    margin="none"
+                                />
+                                {isTimeSelected ?
+                                    <Button
+                                        variant="text"
+                                        onClick={() => handleRemoveStudyTimeSlots(startTime)}
+                                    >
+                                        <CheckIcon />
+                                    </Button>
+                                    :
+                                    <Button
+                                        variant="text"
+                                        onClick={() => handleAddStudyTimeSlots(startTime)}
+                                    >
+                                        <AddIcon />
+                                    </Button>
+                                }
+                            </Stack>
+                        </>
+                    }
+                />
+            );
+        });
+
+
+
+
     const buttons = (
         <React.Fragment>
             <Stack justifyContent="center"
-                   alignItems="center"
-                   spacing={1}
-                   width='100%'
-                   >
-                <PrimaryButton2 width={'97vw'} colour={'#057D78'} content="Add to calendar" onClick={handleEvent}/>
-                <PrimaryButton2 width={'97vw'} colour={'#912338'} content="Cancel" onClick={handleCancel}/>
-                <PrimaryButton2 width={'97vw'} colour={'#0072A8'} content="Dismiss notification" onClick={handleDismiss}/>
+                alignItems="center"
+                spacing={1}
+                width='100%'
+            >
+                <PrimaryButton2 width={'97vw'} colour={'#057D78'} content="Add to calendar" onClick={handleEvent} />
+                <PrimaryButton2 width={'97vw'} colour={'#912338'} content="Cancel" onClick={handleCancel} />
+                <PrimaryButton2 width={'97vw'} colour={'#0072A8'} content="Dismiss notification" onClick={handleDismiss} />
             </Stack>
         </React.Fragment>
     )
@@ -254,21 +303,20 @@ export default function ExamNotification(props){
         <React.Fragment>
             {examName}
             {showAvailability}
-            <div align='center' style={{border:'1px black solid',overflow: 'auto',
+            <div align='center' style={{
+                border: '1px black solid', overflow: 'auto',
                 paddingTop: '10px',
-                marginBottom:'10px',
+                marginBottom: '10px',
                 width: '97vw',
-                height: '35vh'}}>
+                height: '35vh'
+            }}>
                 {timeDisplay}
-                {timeDisplay}
-                {timeDisplay}
-                {timeDisplay}
-                {timeDisplay}
+
             </div>
             {buttons}
 
 
         </React.Fragment>
     )
-    return(examNotification)
+    return (examNotification)
 }
---
+++ src/components/NavDrawer/examNotificationFactory.js
@@ -0,0 +1,85 @@
+import GetAuthentication from "../Authentication/Authentification";
+import axios from 'axios';
+
+
+
+async function getEventsByUsername(user_name, futureTimestamp) {
+  try {
+    console.log(`${process.env.REACT_APP_BASE_URL}events/${user_name}`)
+    
+    // Convert futureTimestamp to UTC time
+    const futureUtcTimestamp = new Date(futureTimestamp).toUTCString();
+    
+    const response = await axios.get(`${process.env.REACT_APP_BASE_URL}events/${user_name}`);
+    console.log(response)
+    
+    const events = response.data.filter((event) => {
+      return new Date(event.startDate) >= new Date() && new Date(event.startDate) <= new Date(futureUtcTimestamp);
+    });
+    
+    const eventTimes = events.map((event) => {
+      const startTime = new Date(event.startTime).toLocaleString("en-US", {timeZone: "America/New_York"});
+      const endTime = new Date(event.endTime).toLocaleString("en-US", {timeZone: "America/New_York"});
+      return [new Date(startTime), new Date(endTime)];
+    });
+    
+    return eventTimes;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+
+
+
+export async function getHoursBetweenTimestamps(startTimestamp, endTimestamp, timeslots, segment) {
+  // Convert start and end timestamps to JavaScript Date objects
+  const username = GetAuthentication().username
+  console.log(username)
+  const excludedTimes = await getEventsByUsername(username, "2023-03-31");
+  console.log(excludedTimes)
+  console.log(excludedTimes)
+  console.log(excludedTimes)
+  const startDate = new Date(startTimestamp);
+  const endDate = new Date(endTimestamp);
+
+  // Round up to the next time segment for the start date
+  startDate.setMinutes(Math.ceil(startDate.getMinutes() / segment) * segment, 0, 0);
+
+  // Create an array to store the results
+  const hoursBetween = [];
+
+  // Loop through each time segment between the start and end dates
+  let currentDate = new Date(startDate);
+  while (currentDate <= endDate) {
+    const currentTimestamp = currentDate.toISOString();
+
+    // Check if the current time segment is excluded
+    let isExcluded = false;
+    for (let i = 0; i < excludedTimes.length; i++) {
+      const startExcluded = new Date(excludedTimes[i][0]);
+      const endExcluded = new Date(excludedTimes[i][1]);
+
+      // Floor the start time and ceil the end time
+      startExcluded.setMinutes(Math.floor(startExcluded.getMinutes() / segment) * segment, 0, 0);
+      endExcluded.setMinutes(Math.ceil(endExcluded.getMinutes() / segment) * segment, 0, 0);
+
+      if (currentDate >= startExcluded && currentDate < endExcluded) {
+        isExcluded = true;
+        break;
+      }
+    }
+
+    // Add the current time segment to the results if it's not excluded
+    if (!isExcluded) {
+      hoursBetween.push(currentTimestamp);
+    }
+
+    // Move to the next time segment
+    currentDate.setMinutes(currentDate.getMinutes() + segment);
+  }
+  timeslots(hoursBetween)
+  return hoursBetween;
+}
+
