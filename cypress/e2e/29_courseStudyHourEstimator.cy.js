d00ab0faee03c301ac3967e195ccd21c84cf62f3
eyalazimov <eyalazimov@gmail.com>
2023-03-29 14:11:25 -0400

    [#15][End-to-end testing] - deleted 6 seconds of wait() in e2e tests

--- cypress/e2e/29_courseStudyHourEstimator.cy.js
+++ cypress/e2e/29_courseStudyHourEstimator.cy.js
@@ -7,12 +7,10 @@ describe('Test Course Study Hour Estimator [SP-29]', function() {
 
     it('Add Course Soen 331', function() {
         cy.addCourseEvent('Soen','331','Intro Formal Methods for Software Engineering','11:45:00','13:00:00',true,'month');
-        cy.wait(2000);
     })
 
     it('Add Course Soen 385', function() {
         cy.addCourseEvent('Soen','385','Applied Systems and Applications','10:15:00','11:30:00',true,'month');
-        cy.wait(2000);
     })
 
     it('Check Study Hours', function(){
@@ -20,7 +18,6 @@ describe('Test Course Study Hour Estimator [SP-29]', function() {
         cy.get('[data-test="Progress Report"]').click();
         //added data-test in CommonResources.js, ProfressReportHome.js
         cy.get('[data-test="totalRecommendedStudyTime"]').should('have.text',' Total recommended study time: 9 hrs');
-        cy.wait(2000)
     })
 
     it('Delete Course Event Soen 331', function(){
