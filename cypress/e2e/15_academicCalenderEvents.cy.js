a7234a0dfe3e7dea3c677930d644f39db9b89478
eyalazimov <eyalazimov@gmail.com>
2023-03-29 13:43:10 -0400

    [#15][End-to-end testing] - added test to confirm event for beginning of spring break - added test to confirm event for deadline to withdrawal from winter semester

--- cypress/e2e/15_academicCalenderEvents.cy.js
+++ cypress/e2e/15_academicCalenderEvents.cy.js
@@ -5,14 +5,21 @@ describe('Test Course Study Hour Estimator [SP-29]', function() {
         cy.login("testEmail_1","testPassword_1");
     })
 
-    it('Confirm start date and end date for Spring Break 2023', function() {
-        cy.get('.react-calendar__navigation__label__labelText').click();
+    it('Confirm start date for Spring Break 2023', function() {
+        cy.get('.react-calendar__navigation__label__labelText').click(); // select month and year title
         cy.get('.react-calendar__year-view__months > :nth-child(2)').click(); //choose Feb
-        cy.get('.react-calendar__month-view__days > :nth-child(29)').click(); // Jan 29
-        cy.get('[data-test="Start of mid-term break"]')
+        cy.get('.react-calendar__month-view__days > :nth-child(29)').click(); // Feb 27
+        cy.get('[data-test="Start of mid-term break"]').should('exist'); // check event exists
     })
 
-    // it('Logout', function() {
-    //     cy.logout();
-    // })
+    it('Confirm deadline for withdrawal from program for winter session 2023', function() {
+        cy.get('.react-calendar__navigation__label__labelText').click(); // select month and year title
+        cy.get('.react-calendar__year-view__months > :nth-child(1)').click(); //choose Feb
+        cy.get('.react-calendar__month-view__days > :nth-child(29)').click(); // Jan 23
+        cy.get('[data-test="Deadline for withdrawal from program for winter session"]').should('exist'); // check event exists
+    })
+
+    it('Logout', function() {
+        cy.logout();
+    })
 })
