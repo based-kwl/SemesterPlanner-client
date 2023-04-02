0981cbc96013dc36c3527a174f02ee1367b3ec3f
eyalazimov <eyalazimov@gmail.com>
2023-04-02 15:07:13 -0400

    [#15][Bug] - modified aspect ratio during test because element was not clickable

--- cypress/e2e/15_academicCalenderEvents.cy.js
+++ cypress/e2e/15_academicCalenderEvents.cy.js
@@ -10,13 +10,15 @@ describe('Test Course Study Hour Estimator [SP-29]', function() {
     })
 
     it('Confirm start date for Spring Break 2023', function() {
-        cy.get('.react-calendar__navigation__label__labelText').click(); // select month and year title
+        cy.viewport(500,1000);
+        cy.get('.react-calendar__navigation__label__labelText').click();
         cy.get('.react-calendar__year-view__months > :nth-child(2)').click(); //choose Feb
         cy.get('.react-calendar__month-view__days > :nth-child(29)').click(); // Feb 27
         cy.get('[data-test="Start of mid-term break"]').should('exist'); // check event exists
     })
 
     it('Confirm deadline for withdrawal from program for winter session 2023', function() {
+        cy.viewport(500,1000);
         cy.get('.react-calendar__navigation__label__labelText').click(); // select month and year title
         cy.get('.react-calendar__year-view__months > :nth-child(1)').click(); //choose Feb
         cy.get('.react-calendar__month-view__days > :nth-child(29)').click(); // Jan 23
